ac71f303ad77787dea244c91a8377395fe3654bd
Luis Mendes <machadoluismendes@gmail.com>
2020-12-28 07:10:11 -0300

    fix
    export { } modules

--- pwa_ionic_react/poligonosApp/src/pages/Tab1.test.tsx
+++ pwa_ionic_react/poligonosApp/src/pages/Tab1.test.tsx
@@ -1,9 +1,10 @@
 const { render } = require("testing-library/react");
-const Tab1 = require("./Tab1");
-// import React from "react";
+import Tab1 from "./Tab1";
+import React from "react";
 
 test("should Tab1", () => {
   const { baseElement } = render(<Tab1 />);
   expect(baseElement).toBeDefined();
   fail(true);
 });
+export {};
