5d94662a197941246b61bc2f8011663b54b7b5a4
Luis Mendes <machadoluismendes@gmail.com>
2021-01-01 06:56:47 -0300

    fs patch
    docker running
    kubernetes running

--- pwa_ionic_react/poligonosApp/webpack.config.js
+++ pwa_ionic_react/poligonosApp/webpack.config.js
@@ -19,6 +19,27 @@ prettier.format("type Query { hello: String }", {
 
 module.exports = () => {
   return {
+	  module: {
+    loaders: [
+      {exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/},
+      {loader: 'style-loader!css-loader', test: /\.css$/},
+      {loader: 'url-loader', test: /\.gif$/},
+      {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
+    },
+    extensions: ['', 'js', 'jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
+  },
     mode: 'production',
     devServer: {
       inline: false,
