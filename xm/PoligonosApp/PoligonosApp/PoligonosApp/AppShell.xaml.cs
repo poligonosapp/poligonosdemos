4ee0e3729641a88f5a779b1318affe2bc6bbecab
Luis Mendes <machadoluismendes@gmail.com>
2021-01-11 01:01:08 -0300

    new xamarin blank app
    PoligonosApp

---
+++ xm/PoligonosApp/PoligonosApp/PoligonosApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PoligonosApp.ViewModels;
+using PoligonosApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PoligonosApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ xm/PoligonosApp/PoligonosApp/PoligonosApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PoligonosApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PoligonosApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
