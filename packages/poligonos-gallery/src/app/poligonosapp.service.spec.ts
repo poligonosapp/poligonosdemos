626c1eb111cd1f66e7d717a6bd75bdbea78ee6d6
Luis Mendes <machadoluismendes@gmail.com>
2021-05-23 11:39:34 -0300

    shared angular components with ionic

---
+++ packages/poligonos-gallery/src/app/poligonosapp.service.spec.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7ba53f18699e1686297f78410c517c97a2a2a0f47192e1717913f28115fae8b0
+size 387
--- packages/poligonos-gallery/src/app/tab1/tab1.page.ts
+++ packages/poligonos-gallery/src/app/tab1/tab1.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a393c3cb12afea02b30f9a1a5c43145253af80dee663baf94f48daab259d014f
-size 1210
+oid sha256:e0b3a6e28ac5b0fb92959bc8a4150be87ad8567c128f1e368fd030569c1c7e64
+size 1700
