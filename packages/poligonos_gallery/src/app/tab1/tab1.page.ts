35ce2b2541535546f915ab94dc65ae6dae7fe987
Luis Mendes <machadoluismendes@gmail.com>
2021-05-26 00:49:25 -0300

    export  shared scripts two ionic angular
    export  shared scripts two ionic angular

--- packages/poligonos_gallery/src/app/tab1/tab1.page.ts
+++ packages/poligonos_gallery/src/app/tab1/tab1.page.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a35c2030f1249df2a3bcb63326d5b8f353bb2c42e9d64502ecc8b9f57f290c13
-size 1734
+oid sha256:15decf928f1a0308c4e4632db44519ceb3a8830d751137a0fcd106f7c79328af
+size 1723
--- packages/poligonos_gallery/src/shared/index.ts
+++ packages/poligonos_gallery/src/shared/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b312f567caae5443bf69b971ccbfb2595c9c3aff40546ed7f347de27b9c758fe
-size 683
+oid sha256:e76b46855343a8b655fa11a121d3de7b7a26cd26967361ceaf38b0c9b9fb7a91
+size 716
