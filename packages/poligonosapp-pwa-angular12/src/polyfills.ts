e91b1364062a16092201939b4f475dd347d7442c
Luis Mendes <machadoluismendes@gmail.com>
2021-05-28 21:55:04 -0300

    Event binding vscode
    https://angular.io/guide/event-binding

--- packages/poligonosapp-pwa-angular12/src/polyfills.ts
+++ packages/poligonosapp-pwa-angular12/src/polyfills.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15cd1ee8564628eac3f1cf9967439be883692f6ef75bbd88b8c0b09949650efc
-size 3064
+oid sha256:6480e896ed6ea1821b6b63e0dc9a9ec0541335313a6586cf7eafa64571947134
+size 3200
