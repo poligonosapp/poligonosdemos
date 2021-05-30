a078a73153822a7ab296f67bfadf1726a20ec16f
Luis Mendes <machadoluismendes@gmail.com>
2021-05-30 03:29:29 -0300

    submodules netlify

--- packages/poligonosapp-pwa-angular12/src/github-actions-parser.ts
+++ packages/poligonosapp-pwa-angular12/src/github-actions-parser.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b3405e195a19d5c15c978646118ff7a0aa6f69fad5cdcf84931be65a829eb8f
-size 600
+oid sha256:b9ed3489235b448525c12c6630612e3c98955dcbd433dcff062b8c9233f92890
+size 660
