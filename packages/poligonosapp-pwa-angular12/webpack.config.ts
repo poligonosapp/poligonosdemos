92e209276d29f74ad02c1b3879195328ef022c67
Luis Mendes <machadoluismendes@gmail.com>
2021-06-07 18:04:09 -0300

    Angular 12 module resolution
    Angular@12.0.1 webpack@5.38.1 resolve fallback module resolution

--- packages/poligonosapp-pwa-angular12/webpack.config.ts
+++ packages/poligonosapp-pwa-angular12/webpack.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef61959e6b0acc661c4029df50bc3e334d2d2f502aed41c9a2082a2767fe46d
-size 325
+oid sha256:729cbc01c5ba5f79aff5f7a265efbcfe35ce3b94e515d8363c434528887ab610
+size 502
