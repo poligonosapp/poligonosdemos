6ed91052f5cc7244754d9db81f5897637e179d4c
Luis Mendes <machadoluismendes@gmail.com>
2021-08-04 11:39:10 -0300

    v1 endpoint
    geojson
    get polygons route

--- packages/meupoligono/app/controllers/application_controller.rb
+++ packages/meupoligono/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
-class ApplicationController < ActionController::Base
-    def index
-        render json: ApplicationController.all
+module Api
+    module V1
+        class ApplicationController < ActionController::Base
+            def index
+                render geojson: { sucessful:true }
+            end
+        end
     end
 end
