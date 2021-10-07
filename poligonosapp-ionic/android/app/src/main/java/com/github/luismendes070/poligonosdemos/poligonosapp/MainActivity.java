21f7adde51ea86b263fc6e0ffc426c15285f6681
Luis Mendes <machadoluismendes@gmail.com>
2021-10-07 16:23:27 -0300

    Android Studio
    ionic
    react native

--- poligonosapp-ionic/android/app/src/main/java/com/github/luismendes070/poligonosdemos/poligonosapp/MainActivity.java
+++ poligonosapp-ionic/android/app/src/main/java/com/github/luismendes070/poligonosdemos/poligonosapp/MainActivity.java
@@ -34,4 +34,5 @@ public class MainActivity extends BridgeActivity {
 
 
   }
+  
 }
---
+++ poligonosapp-ionic/src/components/Poligono.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1e70d35be6f7bf80c02b60cfd4b6b6261d633894ec264159ef9a52ed8f5cf3d
+size 1079
