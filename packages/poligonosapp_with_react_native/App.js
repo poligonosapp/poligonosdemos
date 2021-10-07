2986abe850df737b6ccac7c800d303fd29ae3ac4
Luis Mendes <machadoluismendes@gmail.com>
2021-10-07 13:17:25 -0300

    Android Permissions API 29
    android:installLocation="preferExternal">
    
        <uses-permission android:name="android.permission.INTERNET" />
        <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
        <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
        <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" />

--- packages/poligonosapp_with_react_native/App.js
+++ packages/poligonosapp_with_react_native/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98b27f8c7f855b6cf99dfd160cf4d0f65ff5865bf198bcaaf4cc3f7fd29910c4
-size 2473
+oid sha256:eff6d00c888819247088ea2bc1ed879cb5fd4b1c8f226451d31133966fb472d5
+size 2414
---
+++ packages/poligonosapp_with_react_native/declaration.d.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1637c3b8dd49f710ed15800b891752d9cc6d6fd12f9602d6221336092e0d6b9e
+size 38
