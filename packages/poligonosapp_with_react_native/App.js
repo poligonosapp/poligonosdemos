e9fdf418656aacac48f083507ee31c6e58c9c3e4
Luis Mendes <machadoluismendes@gmail.com>
2021-10-07 21:28:22 -0300

    open file realm
    // Get on-disk location of the default Realm
    Realm.open({}).then(realm => {
        console.log("Realm is located at: " + realm.path);
    });

--- packages/poligonosapp_with_react_native/App.js
+++ packages/poligonosapp_with_react_native/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0afc84260fb11240a7a4199799735a7d37404d6148a43d21089acf5e7d159171
-size 3468
+oid sha256:52dc82ee7d1bb011ff95d4cc382eada4970cbb6d53ad96b69174b6207ee6b088
+size 3723
