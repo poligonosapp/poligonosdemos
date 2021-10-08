e49507471648b9b8110898931411f92550516f77
Luis Mendes <machadoluismendes@gmail.com>
2021-10-07 21:34:56 -0300

    react-native View style

--- packages/poligonosapp_with_react_native/App.js
+++ packages/poligonosapp_with_react_native/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bea3891543d0f6bc114485c83d189c2165b4aa8dcb6d3329c3216a521fd7c69f
-size 3735
+oid sha256:3cb1263e8f481bdbf84d3718617e892160c97a603eec82ae1a07dbbf4cb84d53
+size 4117
