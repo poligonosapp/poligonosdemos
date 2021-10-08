07d1e4927b4c4e0d7d10fd46ffbf4699aeb845af
Luis Mendes <machadoluismendes@gmail.com>
2021-10-07 21:29:49 -0300

    import Button react-native

--- packages/poligonosapp_with_react_native/App.js
+++ packages/poligonosapp_with_react_native/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52dc82ee7d1bb011ff95d4cc382eada4970cbb6d53ad96b69174b6207ee6b088
-size 3723
+oid sha256:bea3891543d0f6bc114485c83d189c2165b4aa8dcb6d3329c3216a521fd7c69f
+size 3735
